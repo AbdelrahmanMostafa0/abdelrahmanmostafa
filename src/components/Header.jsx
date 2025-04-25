6a5dadae023d0db2e1a3be7e223d738e1c979b9f
Abdelrahmanmostafa0 <abdelrahmanmostafa.developer@gmail.com>
2025-04-26 01:58:46 +0300

    style: refactor Header component layout and add download button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ import { BsFillSunFill } from "react-icons/bs";
 import { useEffect, useState } from "react";
 import { RiMoonFill } from "react-icons/ri";
 import { motion } from "motion/react";
+import { FaFileDownload } from "react-icons/fa";
 
 const Header = () => {
   const [isDark, setIsDark] = useState(false);
@@ -30,32 +31,37 @@ const Header = () => {
     audio.play();
   };
   return (
-    <button
-      className="fixed top-5 left-5 text-white z-50 p-1"
-      onClick={() => {
-        playSound();
-        toggleDarkMode();
-      }}
-    >
-      {isDark ? (
-        <motion.div
-          initial={{ rotate: 0 }}
-          animate={{ rotate: 90 }}
-          transition={{ duration: 0.5, repeatType: "reverse" }}
-        >
-          <BsFillSunFill className="text-4xl drop-shadow-lg" />
-        </motion.div>
-      ) : (
-        <motion.div>
-          <RiMoonFill className="text-4xl drop-shadow-lg" />
-        </motion.div>
-      )}
-      {/* {!isDark ? (
+    <div className="flex w-full px-4 fixed top-5 z-50 justify-between items-center">
+      <button
+        className=" left-5 text-white p-1"
+        onClick={() => {
+          playSound();
+          toggleDarkMode();
+        }}
+      >
+        {isDark ? (
+          <motion.div
+            initial={{ rotate: 0 }}
+            animate={{ rotate: 90 }}
+            transition={{ duration: 0.5, repeatType: "reverse" }}
+          >
+            <BsFillSunFill className="text-4xl drop-shadow-lg" />
+          </motion.div>
+        ) : (
+          <motion.div>
+            <RiMoonFill className="text-4xl drop-shadow-lg" />
+          </motion.div>
+        )}
+        {/* {!isDark ? (
           <RiMoonFill className="text-4xl" />
         ) : (
           <BsFillSunFill className="text-4xl" />
         )} */}
-    </button>
+      </button>
+      <a href="/resume.pdf" download="resume.pdf" className="  md:hidden ">
+        <FaFileDownload className="text-3xl text-white drop-shadow-lg " />
+      </a>
+    </div>
   );
 };
 
